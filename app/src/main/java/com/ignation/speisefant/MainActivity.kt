869d4f1718d5ca61c77de90935ce4ae18241f648
Maria <korchagina.adv@gmail.com>
2022-03-08 20:37:45 +0100

    Email intent added. Up button behaviour fixed.

--- app/src/main/java/com/ignation/speisefant/MainActivity.kt
+++ app/src/main/java/com/ignation/speisefant/MainActivity.kt
@@ -23,12 +23,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-        setupActionBarWithNavController(navController)
 
+        setupActionBarWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/ignation/speisefant/fragments/AboutFragment.kt
+++ app/src/main/java/com/ignation/speisefant/fragments/AboutFragment.kt
@@ -1,9 +1,12 @@
 package com.ignation.speisefant.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.ignation.speisefant.R
 
@@ -15,6 +18,21 @@ class AboutFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false)
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button: ImageView = requireView().findViewById(R.id.image_button)
+
+        button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:ignation.app@gmail.com")
+            }
+            if (intent.resolveActivity(activity?.packageManager!!) != null) {
+                startActivity(intent)
+            }
+        }
+    }
 }
